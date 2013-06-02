f0f7012e364c1b105db79d57fe1f4886f2b98025
GeoAvila <geovanny.avila@gmail.com>
2013-06-02 10:33:22 -0600

    Where LInQ

--- Ejercicio5/Program.cs
+++ Ejercicio5/Program.cs
@@ -11,14 +11,19 @@ namespace Ejercicio5
         static void Main(string[] args)
         {
             int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var pares = from n in numeros
-                        where n % 2 == 0
-                        select n;
-            foreach (var n in pares)
-            {
-                Console.WriteLine(n);
-            }
+
+            //var pares = from n in numeros
+            //            where n % 2 == 0
+            //            select n;
+            var pares = numeros.Where(delegate(int n) { return n % 2 == 0; });
+
+            //foreach (var n in pares)
+            //{
+            //    Console.WriteLine(n);
+            //}
+
             Console.ReadKey();
+            
         }
     }
 }
