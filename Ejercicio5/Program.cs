8d4fe2617528437d02c2dee0d1fbb7d99143a8a5
GeoAvila <geovanny.avila@gmail.com>
2013-06-02 11:35:21 -0600

    Dynamic

--- Ejercicio5/Program.cs
+++ Ejercicio5/Program.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Dynamic;
 namespace Ejercicio5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var numeros = Enumerable.Range(1,1000);
+            //var numeros = Enumerable.Range(1,1000);
 
-            //var pares = from n in numeros
-            //            where n % 2 == 0
-            //            select n * n;
-            var pares = numeros.Where(n => n % 2 == 0).ToArray();
-            foreach (var n in pares)
-            {
-                Console.WriteLine(n);
-            }
-            Console.ReadKey();
+            ////var pares = from n in numeros
+            ////            where n % 2 == 0
+            ////            select n * n;
+            //var pares = numeros.Where(n => n % 2 == 0).ToArray();
+            //foreach (var n in pares)
+            //{
+            //    Console.WriteLine(n);
+            //}
+            //Console.ReadKey();
+            dynamic obj = new ExpandoObject();
+            obj.Mipropiedad = "Hola";
+            obj.OtraPropiedad = 3.1416;
+            Console.WriteLine(obj.OtraPropiedad);
             
         }
     }
