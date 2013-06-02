d895f9704cb76b744ec73ff48050b6ad2e5b17a2
GeoAvila <geovanny.avila@gmail.com>
2013-06-02 10:24:08 -0600

    Master

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var pares = from n in numeros
+                        where n % 2 == 0
+                        select n;
+            foreach (var n in pares)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
